897785e1d35caaa32cc8cf20e581bea7c48a04e5
Sebastian Minich <sminich@hm.edu>
2021-05-17 11:08:28 +0200

    update_17.05.21

---
+++ lab0/gabby-cover/nerdlets/gabby-cover-nerdlet/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
+
+export default class GabbyCoverNerdletNerdlet extends React.Component {
+    render() {
+        return <h1>Hello, gabby-cover-nerdlet Nerdlet!</h1>;
+    }
+}
